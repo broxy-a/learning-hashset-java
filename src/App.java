f1ed263e406b061a373fa5f4d269209b6979301a
broxy-a <abdrahmanovamir2001@gmail.com>
2025-02-27 17:06:35 +0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 import java.util.HashSet;
 
 public class App {
+
+  // Хранит имена избирателей, которые уже голосовали
   private static HashSet<String> voters = new HashSet<String>();
+
+  // Проверяет, голосовал человек или нет. Если голосует впервые — добавляет в список и выводит сообщение.
 public static void checkVoter(String name) {
     if (voters.contains(name)) {  
         System.out.println("Этот человек уже голосовал");  
@@ -10,7 +14,11 @@ public static void checkVoter(String name) {
         System.out.println("Этот человек голосует впервые");
     }
 }
+
+  // Тест программы и точка входа в программу.
 public static void main(String[] args) {   
+
+    // Проверяем избирателей. Один из них голосует дважды
     checkVoter("Artorias"); 
     checkVoter("Aiku"); 
     checkVoter("Artorias");
